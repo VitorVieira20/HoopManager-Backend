3de7662989784c2830cb528274156f98b2a2a956
Vitor Vieira <2078320@student.uma.pt>
2024-06-17 17:59:58 +0100

    feat: add more entities

--- api/src/main/java/com/hoopmanger/api/domain/club/Club.java
+++ api/src/main/java/com/hoopmanger/api/domain/club/Club.java
@@ -1,26 +1,36 @@
-package com.hoopmanger.api.domain.user;
+package com.hoopmanger.api.domain.club;
 
+import com.hoopmanger.api.domain.team.Team;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
-@Table( name= "tb_users" )
+@Table( name= "tb_clubs" )
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class Club {
     @Id
     @GeneratedValue
     private UUID id;
+
+    private long phone;
 
     private String name;
 
     private String email;
 
-    private String password;
+    private String instagram;
+
+    private String facebook;
+
+    private String twitter;
 }
--- api/src/main/java/com/hoopmanger/api/domain/team/Team.java
+++ api/src/main/java/com/hoopmanger/api/domain/team/Team.java
@@ -1,26 +1,29 @@
-package com.hoopmanger.api.domain.user;
+package com.hoopmanger.api.domain.team;
 
+import com.hoopmanger.api.domain.club.Club;
+import com.hoopmanger.api.domain.player.Player;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import java.util.UUID;
 
 @Entity
-@Table( name= "tb_users" )
+@Table( name= "tb_teams" )
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class Team {
     @Id
     @GeneratedValue
     private UUID id;
 
     private String name;
 
-    private String email;
-
-    private String password;
+    @ManyToOne
+    @JoinColumn(name = "club_id", nullable = false)
+    private Club club;
 }
