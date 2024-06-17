b8f41b9a0080bde811a67fdf6f0ab4680f8bb01b
Vitor Vieira <2078320@student.uma.pt>
2024-06-17 11:19:48 +0100

    feat: create User entity and add repository

---
+++ api/src/main/java/com/hoopmanger/api/domain/user/User.java
@@ -0,0 +1,23 @@
+package com.hoopmanger.api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.UUID;
+
+@Entity
+@Table( name= "tb_users" )
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+}
