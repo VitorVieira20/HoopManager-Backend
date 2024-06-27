2dec4fc5dac0a5970781cec4483eb869892814b6
Vitor Vieira <2078320@student.uma.pt>
2024-06-27 11:59:17 +0100

    feat: implement a simple logger to log the request from the client

---
+++ src/main/java/com/hoopmanger/api/infra/logger/RequestLoggingInterceptor.java
@@ -0,0 +1,35 @@
+package com.hoopmanger.api.infra.logger;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestLoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        logger.info("Incoming request: {} {} from {}", request.getMethod(), request.getRequestURI(), request.getRemoteAddr());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        // Você pode adicionar logs adicionais aqui, se necessário
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        if (ex != null) {
+            logger.error("Request completed with error: {}", ex.getMessage());
+        } else {
+            logger.info("Request completed successfully");
+        }
+    }
+}
